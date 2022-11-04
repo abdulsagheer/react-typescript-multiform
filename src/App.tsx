d5e19d2967d2ed3bbc74da808ecad8e084710d39
abdulsagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-11-04 20:03:31 +0530

    Added Components in main section for multistep form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import { useMultiStepForm } from "./hooks/useMultistepForm";
+import UserForm from "./components/UserForm";
+import AddressForm from "./components/AddressForm";
+import AccountForm from "./components/AccountForm";
+import { FormEvent } from "react";
 
 function App() {
   const { steps, currentStepIndex, step, back, next } = useMultiStepForm([
-    <div>One</div>,
-    <div>Two</div>,
-    <div>Three</div>,
+    <UserForm />,
+    <AddressForm />,
+    <AccountForm />,
   ]);
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    next();
+  }
 
   return (
     <div
@@ -21,7 +30,7 @@ function App() {
       }}
     >
       <h1>React Multiform</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div
           style={{
             position: "absolute",
@@ -32,7 +41,7 @@ function App() {
         >
           {currentStepIndex + 1} / {steps.length}
         </div>
-        {step}
+        <div style={{ minHeight: "300px" }}>{step}</div>
       </form>
       <div
         style={{
@@ -53,14 +62,14 @@ function App() {
           Back
         </button>
         <button
+          type="submit"
+          onClick={() => next()}
           style={{
             padding: "0.5rem 1rem",
             color: "white",
           }}
-          onClick={() => next()}
-          disabled={currentStepIndex === steps.length - 1}
         >
-          Next
+          {currentStepIndex === steps.length - 1 ? "Finish" : "Next"}
         </button>
       </div>
     </div>
